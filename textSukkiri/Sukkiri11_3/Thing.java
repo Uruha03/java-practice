587e322e4cb2b7ee53e7a82b3a19cd8d5a3a253c
Uruha03 <uruha1633@gmail.com>
2026-07-30 17:33:12 +0900

    fix. スッキリ課題11-3を修正

--- textSukkiri/Sukkiri11_3/Thing.java
+++ textSukkiri/Sukkiri11_3/Thing.java
@@ -2,9 +2,7 @@ package textSukkiri.Sukkiri11_3;
 
 public interface Thing {
     // フィールドは暗黙的にpublic static finalになる
-    double weight = 0.0;
-
     // メソッドは暗黙的にpublic abstractになる
     public double getWeight();
-    public double setWeight(double weight);
+    public void setWeight(double weight);
 }
