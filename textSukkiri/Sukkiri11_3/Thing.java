84085ec71d317c66c475a5606c6ef16ee8b997e2
Uruha03 <uruha1633@gmail.com>
2026-07-30 17:12:11 +0900

    feat. スッキリ課題11-3を提出

---
+++ textSukkiri/Sukkiri11_3/Thing.java
@@ -0,0 +1,10 @@
+package textSukkiri.Sukkiri11_3;
+
+public interface Thing {
+    // フィールドは暗黙的にpublic static finalになる
+    double weight = 0.0;
+
+    // メソッドは暗黙的にpublic abstractになる
+    public double getWeight();
+    public double setWeight();
+}
