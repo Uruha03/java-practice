cfbaf23f559ac7d245b04d5ece40013109c9a3c7
Uruha03 <uruha1633@gmail.com>
2026-07-31 16:47:54 +0900

    feat. スッキリ課題13-4で扱うファイルを追加

---
+++ textSukkiri/Sukkiri13_4/Hero.java
@@ -0,0 +1,13 @@
+package textSukkiri.Sukkiri13_4;
+
+public class Hero {
+    private String name;
+    private int hp;
+
+    // ゲッタ
+    public String getName() { return name; }
+    public int getHp() { return hp; }
+    // セッタ
+    public void setName(String name) { this.name = name; }
+    public void setHp(int hp) { this.hp = hp; }
+}
