8a873a3ee2e4885af229dcd422e776507531425e
Uruha03 <uruha1633@gmail.com>
2026-07-31 16:05:27 +0900

    feat. スッキリ課題13-1を提出

---
+++ textSukkiri/Sukkiri13_1/Wand.java
@@ -0,0 +1,13 @@
+package textSukkiri.Sukkiri13_1;
+
+public class Wand {
+	private String name;
+	private double power;
+
+	// ゲッタ
+	public String getName() { return name; }
+	public double getPower() { return power; }
+	// セッタ
+	public void setName(String name) { this.name = name; }
+	public void setPower(double power) { this.power = power; }
+}
---
+++ textSukkiri/Sukkiri13_1/Wizard.java
@@ -0,0 +1,27 @@
+package textSukkiri.Sukkiri13_1;
+
+public class Wizard {
+	private int hp;
+	private int mp;
+	private String name;
+	private Wand wand;
+
+	void heal(Hero h) {
+		int basePoint = 10; //基本回復ポイント
+		int recovPoint = (int)(basePoint * wand.getPower()); //杖による増幅
+		h.setHp(h.getHp() + recovPoint);
+		System.out.println( h.getName() + "のHPを" + recovPoint + "回復した！");
+	}
+
+	// ゲッタ
+	public String getName() { return name; }
+	public int getMp() { return mp; }
+	public int getHp() { return hp; }
+	public Wand getWand() { return wand; }
+	// セッタ
+	public void setName(String name) { this.name = name; }
+	public void setMp(int mp) { this.mp = mp; }
+	public void setHp(int hp) { this.hp = hp; }
+	public void setWand (Wand wand) { this.wand = wand; }
+}
+
